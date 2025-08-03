b7a2c554de55787a1b2f94b2074952f2e1a076a5
RC46 <gaboac46@gmail.com>
2025-08-03 02:08:09 -0500

    Todas las dependencias necesarias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', // Tipo de base de datos
+      host: 'localhost', // Dirección del servidor MySQL (o configuración personalizada)
+      port: 3306, // Puerto por defecto de MySQL
+      username: 'root', // Usuario de MySQL
+      password: 'e4c5najdorf', // Contraseña de MySQL
+      database: 'parking', // Nombre de la base de datos
+      entities: [], // Aquí irán las entidades más tarde
+      synchronize: true, // Sincroniza las entidades con la base de datos (en desarrollo)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
